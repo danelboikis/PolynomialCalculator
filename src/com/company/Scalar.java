c8c38e9764f21c36087fb8ec99fd4817f01bd427
Danel Boikis <danelbo@post.bgu.ac.il>
2022-04-10 14:33:35 +0300

    version 1

--- src/com/company/Scalar.java
+++ src/com/company/Scalar.java
@@ -1,5 +1,13 @@
 package com.company;
 
 public interface Scalar {
-    public 
+    Scalar add(Scalar s);
+
+    Scalar mul(Scalar s);
+
+    Scalar neg();
+
+    Scalar power(int exponent);
+
+    int sign();
 }
