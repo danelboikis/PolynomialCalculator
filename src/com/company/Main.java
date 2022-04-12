57ced4580e2dbd89d7724131c47447d4b23f5679
michaeli123 <adarmic@post.bgu.ac.il>
2022-04-12 15:39:12 +0300

    version 7 michael

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,6 +8,14 @@ public class Main {
         Scalar s1 = new Rational(-30, 13);
         Scalar s2 = new Integer(-5);
 
-        System.out.println(s1.mul(s2));
+        Monomial m1 = new Monomial(3, s1);
+        Monomial m2 = new Monomial(3, s2);
+
+
+        System.out.println(m1.toString());
+        System.out.println(m1.mul(m2).toString());
+        System.out.println(m1.add(m2).toString());
+        System.out.println(m1.derivative().toString());
+
     }
 }
---
+++ src/com/company/Polynomial.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.util.LinkedList;
+
+public class Polynomial {
+    private LinkedList<Monomial> monomials;
+
+    public Polynomial()
+    {
+        monomials = new LinkedList<>();
+    }
+}
