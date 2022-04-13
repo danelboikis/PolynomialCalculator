4915e8853993f3ac29f52fddd76f97b50f4e1798
Danel Boikis <danelbo@post.bgu.ac.il>
2022-04-13 12:19:49 +0300

    version 6

--- src/com/company/Polynomial.java
+++ src/com/company/Polynomial.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import java.util.Map;
 import java.util.TreeMap;
 
 public class Polynomial {
@@ -9,6 +10,19 @@ public class Polynomial {
     {
         monomials = new TreeMap<>();
         monomials.put("0", new Monomial(0, new Integer(0)));
+    }
+
+    public Polynomial add(Polynomial p) {
+        Polynomial res = new Polynomial();
+
+        for (Map.Entry<String, Monomial> entry:
+             monomials.entrySet()) {
+            if(res.monomials.containsKey(entry.getKey())) {
+                //res.monomials.put(entry.getKey(), )
+            }
+        }
+
+        return null;
     }
 
 
