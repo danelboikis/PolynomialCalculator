bb99b4b5af13cbd8f726c5d0776fe467128c8b25
Danel Boikis <danelbo@post.bgu.ac.il>
2022-04-14 19:03:44 +0300

    version 13 danel

--- Tests/Calculator/RationalTest.java
+++ Tests/Calculator/RationalTest.java
@@ -1,5 +1,6 @@
 package Calculator;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,8 +9,16 @@ class RationalTest {
     private Scalar r;
     private Rational r2;
 
+    @BeforeEach
+    void setUp() {
+        r = new Rational(1, 2);
+        r2 = new Rational(1, 2);
+    }
+
     @Test
     void add() {
+        r = r.add(new Rational(0, 1));
+        assertEquals(r ,new Rational(0, 1));
     }
 
     @Test
